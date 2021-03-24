56974be46689bd7bd127d60630cf92cee581e8cb
J Edwards <jesse.edwards2@protonmail.com>
2021-03-24 09:28:28 -0400

    the database is running on port 4000

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,24 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
+import { dbURI, port } from './config/environment.js'
 const app = express()
+
+app.use((req, _res, next) => {
+  console.log(`incoming requests: ${req.method} - ${req.url}`)
+  next()
+})
+
+app.use(express.json())
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+    console.log('THE DATABASE IS CONNECTED!!!!!')
+  } catch (error) {
+    console.log('something has gone wrong. see below for info')
+    console.log(error)
+  }
+  app.listen(port, () => console.log(`express is running on port ${port}`))
+}
+
+startServer()
