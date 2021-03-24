2f1c4343691a79a2da9cbe1a17b1e93a2f51a708
Harry <harryfrwarwick@gmail.com>
2021-03-24 16:08:05 +0000

    3 pubs seeds

---
+++ backend/db/data/pubSeeds.js
@@ -0,0 +1,82 @@
+export default [
+  {
+    nameOfPub: '19:20',
+    address: {
+      line1: '19-20 Great Sutton Street',
+      line2:'Islington', 
+      town:'',
+      city: 'London',
+      postCode: 'EC1V 0NB'
+    },
+    description: '' ,
+    isOutsideSeating: true,
+    isPetFriendly: true,
+    isFoodServed: true,
+    isLiveSports: true,
+    image: 'http://www.flickr.com/photos/55935853@N00/7668655638/'
+  },
+  {
+    nameOfPub: 'Southampton Arms',
+    address: {
+      line1: '139' ,
+      line2: 'Highgate Rd',
+      town: '',
+      city: 'London',
+      postCode: 'NW5 1LE'
+    },
+    description: 'The sign outside announces ‘ale, cider, meat’, and that pretty much sums up what’s on offer at the best pub in Gospel Oak. The interior might make you think it’s still 1936, as would the cash-only policy. The addition of cheese and charcuterie boards to the offering of meaty bar snacks is about as modern as it gets here. The roster of lovingly tended ales and ciders, all from small independent producers, changes regularly – but Dark Star, Mighty Oak, Moor and Sambrooks are among the names you might encounter. There’s also mulled cider. One of the many great things about the Southampton is that it retains its mixed clientele, long-time residents and newcomers alike – and of all ages. There’s also a pub dog. This is a true local hero.',
+    isOutsideSeating: false,
+    isPetFriendly: true,
+    isFoodServed: true,
+    isLiveSports: false,
+    image: 'https://beersmanchester.files.wordpress.com/2013/05/southampton-arms4.jpg'
+  },
+  {
+    nameOfPub: 'French House',
+    address: {
+      line1: '49',
+      line2: 'Dean Street',
+      town: '',
+      city: 'London',
+      postCode: 'W1D 5BG'
+    },
+    description: 'If you like to drink in places where the history’s as important as the drinks list, the Grade II-listed French House should be on your list. Charles de Gaulle used the pub as a workplace during World War II; Dylan Thomas and Francis Bacon both drank here… the list goes on. The interior reflects both the Gallic connection and its long standing in this most exciting of London neighbourhoods. It’s small, memento-filled, and very crowded when the place is busy. You may end up joining the regulars on the pavement outside, but that’s just part of the French House experience. Food is served during the daytime only, and beer, famously, is served only in halves. This is Soho history. Come and suck it up.',
+    isOutsideSeating: true,
+    isPetFriendly: true,
+    isFoodServed: true,
+    isLiveSports: false,
+    image: 'https://media.timeout.com/images/100684369/750/422/image.jpg'
+  },
+  {
+    nameOfPub: ,
+    address: {
+      line1: ,
+      line2: ,
+      town: ,
+      city: ,
+      postCode: 
+    },
+    description: ,
+    isOutsideSeating: ,
+    isPetFriendly: ,
+    isFoodServed: ,
+    isLiveSports: 
+    image: 
+  }
+  {
+    nameOfPub: ,
+    address: {
+      line1: ,
+      line2: ,
+      town: ,
+      city: ,
+      postCode: 
+    },
+    description: ,
+    isOutsideSeating: ,
+    isPetFriendly: ,
+    isFoodServed: ,
+    isLiveSports: 
+    image: 
+  }
+]
