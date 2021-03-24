50564bde6b27e92e47c0e570ad5703cf0d67d2f3
Harry <harryfrwarwick@gmail.com>
2021-03-24 17:25:22 +0000

    10 pub seeds

--- backend/db/data/pubSeeds.js
+++ backend/db/data/pubSeeds.js
@@ -24,7 +24,7 @@ export default [
       city: 'London',
       postCode: 'NW5 1LE'
     },
-    description: 'The sign outside announces ‘ale, cider, meat’, and that pretty much sums up what’s on offer at the best pub in Gospel Oak. The interior might make you think it’s still 1936, as would the cash-only policy. The addition of cheese and charcuterie boards to the offering of meaty bar snacks is about as modern as it gets here. The roster of lovingly tended ales and ciders, all from small independent producers, changes regularly – but Dark Star, Mighty Oak, Moor and Sambrooks are among the names you might encounter. There’s also mulled cider. One of the many great things about the Southampton is that it retains its mixed clientele, long-time residents and newcomers alike – and of all ages. There’s also a pub dog. This is a true local hero.',
+    description: 'The sign outside announces ‘ale, cider, meat’, and that pretty much sums up what’s on offer at the best pub in Gospel Oak. The interior might make you think it’s still 1936, as would the cash-only policy. The addition of cheese and charcuterie boards to the offering of meaty bar snacks is about as modern as it gets here. The roster of lovingly tended ales and ciders, all from small independent producers, changes regularly. There’s also mulled cider. One of the many great things about the Southampton is that it retains its mixed clientele, long-time residents and newcomers alike – and of all ages. There’s also a pub dog. This is a true local hero.',
     isOutsideSeating: false,
     isPetFriendly: true,
     isFoodServed: true,
@@ -62,7 +62,7 @@ export default [
     isFoodServed: true,
     isLiveSports: false,
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Chesham_Arms.jpg/240px-Chesham_Arms.jpg'
-  }
+  },
   {
     nameOfPub: 'The Mayflower',
     address: {
@@ -78,5 +78,149 @@ export default [
     isFoodServed: true,
     isLiveSports: false,
     image: 'https://cdn.diffords.com/contrib/bar/2020/02/5e4c0712c1378.jpg'
+  },
+  {
+    nameOfPub: 'The Ivy House',
+    address: {
+      line1: '40 Stuart Road',
+      line2: '',
+      town: '',
+      city: 'London',
+      postCode: 'SE15 3BE' 
+    },
+    description: 'If this pub were a film, it would be a heartwarming tale of pluck directed by Mike Leigh. In 2012, the owners gave their tenants one week’s notice to ship out after selling the property to developers. Instead of going quietly, the tenants set about raising £1 million with the help of the local community to buy the pub and turn it into a cooperative. Nowadays friendly, committed staff welcome locals of all ages for a mates-rates pint of locally brewed ale, carefully sourced pub food (including generous Sunday roasts), and events ranging from live music to yoga classes, knitting circles and kids’ dance lessons. It’s all housed in a Grade-II listed building, with original beams, wall-to-wall wood panelling, leaded windows and an open fire.',
+    isOutsideSeating: true,
+    isPetFriendly: true,
+    isFoodServed: false,
+    isLiveSports: false,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Ivy_House%2C_Nunhead%2C_SE15_%285478867910%29.jpg/220px-Ivy_House%2C_Nunhead%2C_SE15_%285478867910%29.jpg'
+  },
+  {
+    nameOfPub: 'Compton Arms',
+    address: {
+      line1: '4 Compton Avenue',
+      line2: '',
+      town: '',
+      city: 'London',
+      postCode: 'N1 2XD' 
+    },
+    description: 'George Orwell loved pubs so much he wrote an essay about them. ‘The Moon Under Water’ was his ode to a fictitious pub that represented the best in boozing culture – a joint down a side street with handsome Victorian features, barmaids who knew your name and plenty of room to bend an elbow. In reality, Orwell was often found drinking at The Compton Arms in Islington. And 70-odd years on from that essay, his favoured pub now merits an essay of its own. In keeping with the considerate refurb, traditional ales remain on cask – Sambrook’s Wandle and Adnams Ghost Ship side by side. The rest is all modern: London craft beers from the likes of Five Points and Partizan; a small but thoughtful wine list (a £6.50 glass of Argentinian malbec was full of juicy red fruit and vanilla flavours); a bold range of whiskies and mezcals; and a short cocktail list with three twists on the negroni. Prices are fair, particularly for this well-to-do area.',
+    isOutsideSeating: true,
+    isPetFriendly: true,
+    isFoodServed: true,
+    isLiveSports: true,
+    image: 'https://assets.londonist.com/uploads/2015/07/i875/img_1803.jpg'
+  },
+  {
+    nameOfPub: 'Pineapple',
+    address: {
+      line1: '51 Leverton Street',
+      line2: '',
+      town: '',
+      city: 'London',
+      postCode: 'NW5 2NX' 
+    },
+    description: 'So great is Kentish Town’s love of the Pineapple that when the pub was threatened with redevelopment into flats in 2002, locals started a campaign to save it – and got the place listed by English Heritage in just eight days. This backstreet boozer dates back to the mid-nineteenth century, and you can feel the history in the weathered wood of the joyously unmodernised main bar. There’s also a conservatory at the back, and outdoor tables when the weather’s good. The Pineapple hosts a variety of special events, including semi-annual beer festivals, but it’s at its best on an ordinary weekend, when the locals crowd in for a pint and a gab-fest. And there’s cheap Thai food if you want to settle in for the long haul.',
+    isOutsideSeating: true,
+    isPetFriendly: true,
+    isFoodServed: true,
+    isLiveSports: false,
+    image: 'https://images.squarespace-cdn.com/content/v1/59c3cdeaedaed82e2b8d7ce1/1506008140270-0EEB5QLUO6L8VQTIY77V/ke17ZwdGBToddI8pDm48kIY_TNWBc1qRH3Eu3XBCQQp7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UYHKwrlcGC3elbGv2d6ch4xBMDaWcbj4BzpYNMDNdVtDZB8_xpyZX_lkIhacCkJq3w/front.jpg'
+  },
+  {
+    nameOfPub: 'Auld Shillelagh',
+    address: {
+      line1: '',
+      line2: '',
+      town: '',
+      city: 'London',
+      postCode: '' 
+    },
+    description: 'The added-value attractions at this skinny little Church Street boozer are numerous: sporadic themed music nights, big-screen football, large beer garden. But the Shillelagh is at its best when it keeps things simple, as an honest, uncomplicated Irish pub (as opposed to Irish-themed pub; the difference is crucial) that draws a devoted circle of boozed-up Stoke Newingtonians most nights of the week. It’s the kind of pub where the wine list runs to ‘red or white’, and where the staff are so matey and hospitable that they’ll offer to bring your Guinness over to your table rather than have you hang around at the bar and wait for it to reach perfection. No wonder everybody here always seems to be in such a good mood.',
+    isOutsideSeating: true,
+    isPetFriendly: true,
+    isFoodServed: true,
+    isLiveSports: false,
+    image: ''
+  }
+  {
+    nameOfPub: '',
+    address: {
+      line1: '',
+      line2: '',
+      town: '',
+      city: 'London',
+      postCode: '' 
+    },
+    description: '',
+    isOutsideSeating: true,
+    isPetFriendly: true,
+    isFoodServed: true,
+    isLiveSports: false,
+    image: ''
+  }
+  {
+    nameOfPub: '',
+    address: {
+      line1: '',
+      line2: '',
+      town: '',
+      city: 'London',
+      postCode: '' 
+    },
+    description: '',
+    isOutsideSeating: true,
+    isPetFriendly: true,
+    isFoodServed: true,
+    isLiveSports: false,
+    image: ''
+  }
+  {
+    nameOfPub: '',
+    address: {
+      line1: '',
+      line2: '',
+      town: '',
+      city: 'London',
+      postCode: '' 
+    },
+    description: '',
+    isOutsideSeating: true,
+    isPetFriendly: true,
+    isFoodServed: true,
+    isLiveSports: false,
+    image: ''
+  }
+  {
+    nameOfPub: '',
+    address: {
+      line1: '',
+      line2: '',
+      town: '',
+      city: 'London',
+      postCode: '' 
+    },
+    description: '',
+    isOutsideSeating: true,
+    isPetFriendly: true,
+    isFoodServed: true,
+    isLiveSports: false,
+    image: ''
+  }
+  {
+    nameOfPub: '',
+    address: {
+      line1: '',
+      line2: '',
+      town: '',
+      city: 'London',
+      postCode: '' 
+    },
+    description: '',
+    isOutsideSeating: true,
+    isPetFriendly: true,
+    isFoodServed: true,
+    isLiveSports: false,
+    image: ''
   }
 ]
