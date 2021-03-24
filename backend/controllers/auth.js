35e9d5545225e3dc8dfeb1b19ce9d96e6ed56beb
J Edwards <jesse.edwards2@protonmail.com>
2021-03-24 17:01:27 -0400

    fixing linter errors

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,12 +1,13 @@
 import User from '../models/user.js'
 
-export const registerUser = async () (req, res) => {
+export const registerUser = async (req, res) => {
 
   try {
 
     const newUser = await User.create(req.body)
-    return res.status(202).json({ message: `Welcome ${ newUser.username }`})
+    return res.status(202).json({ message: `Welcome ${ newUser.username }` })
   } catch (error) {
-  console.log('🚀 ~ file: auth.js ~ line 12 ~ registerUser ~ error', error)
-  return res.stauts(422).json(error)
+    console.log('🚀 ~ file: auth.js ~ line 12 ~ registerUser ~ error', error)
+    return res.stauts(422).json(error)
+  }
 }
