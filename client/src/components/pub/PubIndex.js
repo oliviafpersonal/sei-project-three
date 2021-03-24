e636f0184a83432b6d88d0a7104210f370e8e8af
oliviaf <olivia.flynn2@hotmail.com>
2021-03-24 17:30:13 +0000

    pub index

---
+++ client/src/components/pub/PubIndex.js
@@ -0,0 +1,34 @@
+import React, { /*useState, useEffect */ } from 'react'
+//import axios from 'axios'
+
+//import PubCard from './PubCard'
+
+const PubIndex = () => {
+  /*
+  const [pubs, setPubs] = useState('')
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get('')
+      setPubs(data)
+    }
+    getData()
+  }, [])
+  */
+
+  return (
+    <div>
+      <h2>Pubs in ${location}</h2>
+      <div>
+
+      </div>
+
+
+
+    </div>
+
+  )
+
+}
+
+export default PubIndex
