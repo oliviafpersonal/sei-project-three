c2f2bbeb2aca6d49100df96aafa4c0136ba10fc1
oliviaf <olivia.flynn2@hotmail.com>
2021-03-24 16:35:52 +0000

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './components/Home/Home'
 
-function App() {
-  return <h1>Hello World</h1>
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App
