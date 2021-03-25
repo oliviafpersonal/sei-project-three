8201d077a69d5b8fca89b89bc1cba481b58af7fc
J Edwards <jesse.edwards2@protonmail.com>
2021-03-25 07:44:33 -0400

    back end to frontend connected

---
+++ client/src/components/pub/PubIndex.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const PubIndex = () => {
+  const [pubs, setPubs] = useState(null)
+  useEffect( async () => {
+    const { data } = await axios.get('/api/pubs')
+    console.log(data)
+    setPubs(data)
+  }, [])
+  
+
+  if (!pubs) return null
+  return (
+    <>
+      {console.log(pubs)}
+      <div>
+        I am the Pubs index, welcome
+      </div>
+      <Link to="/"><button>homePage</button></Link>
+    </>
+  )
+}
+
+export default PubIndex
