8a7380c2566aa20922d419a76a65f0042fc2cefc
atilla-arslan <atillaa@rocketmail.comm>
2021-03-25 11:08:36 +0000

    nearby pubs section

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,36 +2,34 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="footer-container">
-      <footer>
-        <div className="footer-body">
-          <div className="columns">
-            <div className="column">
-              <p>
-                <b>ABOUT</b>
-              </p>
+    <footer>
+      <div className="footer-body">
+        <div className="columns">
+          <div className="column">
+            <p>
+              <b>ABOUT</b>
+            </p>
 
-              <ul>
-                <li>How Pubhub works</li>
-                <li>Pubhub Plus</li>
-                <li>Pubhub for work</li>
-                <li>Careers</li>
-              </ul>
-            </div>
-            <div className="column">
-              <p>
-                <b>COMMUNITY</b>
-              </p>
-            </div>
-            <div className="column">
-              <p>
-                <b>LANDLORD</b>
-              </p>
-            </div>
+            <ul>
+              <li>How Pubhub works</li>
+              <li>Pubhub Plus</li>
+              <li>Pubhub for work</li>
+              <li>Careers</li>
+            </ul>
+          </div>
+          <div className="column">
+            <p>
+              <b>COMMUNITY</b>
+            </p>
+          </div>
+          <div className="column">
+            <p>
+              <b>LANDLORD</b>
+            </p>
           </div>
         </div>
-      </footer>
-    </div>
+      </div>
+    </footer>
   )
 }
 
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -5,6 +5,27 @@ const Home = () => {
   return (
     <div className="home-container">
       <Header />
+      <main>
+        <div className="content-container">
+          <div className="pubs-heading">
+            <h1>Pubs Nearby</h1>
+          </div>
+          <div className="columns">
+            <div className="column">
+              <img src="https://www.telegraphmoorgate.co.uk/-/media/sites/microsites/t/the-telegraph-_-p112/images/2019-ray/drink-gallery/01-telegraph-17092019-93.ashx?w=1024"></img>
+              <p>Cool Pubs</p>
+            </div>
+            <div className="column">
+              <img src="https://static.standard.co.uk/s3fs-public/thumbnails/image/2018/12/06/11/the-old-bank-of-england-15.jpg?width=990"></img>
+              <p>Old pubs</p>
+            </div>
+            <div className="column">
+              <img src="https://static.standard.co.uk/s3fs-public/thumbnails/image/2020/02/11/13/ship-shovell-pub.jpg?width=1368"></img>
+              <p>Cute Pubs</p>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
