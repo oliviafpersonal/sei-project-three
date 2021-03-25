6a2d346431fb46e7eee238468818a39f7465ab77
oliviaf <olivia.flynn2@hotmail.com>
2021-03-25 12:08:24 +0000

    appjs-routed

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Footer = () => {
+<<<<<<< HEAD
 
 
 
@@ -9,6 +10,37 @@ const Footer = () => {
     <div>
 
     </div>
+=======
+  return (
+    <footer>
+      <div className="footer-body">
+        <div className="columns">
+          <div className="column">
+            <p>
+              <b>ABOUT</b>
+            </p>
+
+            <ul>
+              <li>How Pubhub works</li>
+              <li>Pubhub Plus</li>
+              <li>Pubhub for work</li>
+              <li>Careers</li>
+            </ul>
+          </div>
+          <div className="column">
+            <p>
+              <b>COMMUNITY</b>
+            </p>
+          </div>
+          <div className="column">
+            <p>
+              <b>LANDLORD</b>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+>>>>>>> 8201d077a69d5b8fca89b89bc1cba481b58af7fc
   )
 }
 
--- client/src/components/pub/PubIndex.js
+++ client/src/components/pub/PubIndex.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { /*useState, useEffect */ } from 'react'
 //import axios from 'axios'
 
@@ -32,3 +33,31 @@ const PubIndex = () => {
 }
 
 export default PubIndex
+=======
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const PubIndex = () => {
+  const [pubs, setPubs] = useState(null)
+  useEffect( async () => {
+    const { data } = await axios.get('/api/pubs')
+    console.log(data)
+    setPubs(data)
+  }, [])
+  
+
+  if (!pubs) return null
+  return (
+    <>
+      {console.log(pubs)}
+      <div>
+        I am the Pubs index, welcome
+      </div>
+      <Link to="/"><button>homePage</button></Link>
+    </>
+  )
+}
+
+export default PubIndex
+>>>>>>> 8201d077a69d5b8fca89b89bc1cba481b58af7fc
