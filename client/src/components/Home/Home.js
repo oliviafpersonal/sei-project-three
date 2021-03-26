a27997f159d3110e5d622f8939edf9ab3bb4e262
oliviaf <olivia.flynn2@hotmail.com>
2021-03-26 16:50:54 +0000

    homepage-routing

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import Header from './Header'
 import StickyHeader from '../StickyHeader'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   // const [ isSticky, setIsSticky ] = useState(false)
@@ -35,16 +36,24 @@ const Home = () => {
           </div>
           <div className="columns">
             <div className="column">
-              <img src="https://www.telegraphmoorgate.co.uk/-/media/sites/microsites/t/the-telegraph-_-p112/images/2019-ray/drink-gallery/01-telegraph-17092019-93.ashx?w=1024"></img>
-              <p>Cool Pubs</p>
+              <Link to="/pubs">
+                <img src="https://www.telegraphmoorgate.co.uk/-/media/sites/microsites/t/the-telegraph-_-p112/images/2019-ray/drink-gallery/01-telegraph-17092019-93.ashx?w=1024"></img>
+                <p>Cool Pubs</p>
+              </Link>
+
             </div>
             <div className="column">
-              <img src="https://static.standard.co.uk/s3fs-public/thumbnails/image/2018/12/06/11/the-old-bank-of-england-15.jpg?width=990"></img>
-              <p>Old pubs</p>
+              <Link to="/pubs">
+                <img src="https://static.standard.co.uk/s3fs-public/thumbnails/image/2018/12/06/11/the-old-bank-of-england-15.jpg?width=990"></img>
+                <p>Old pubs</p>
+              </Link>
+
             </div>
             <div className="column">
-              <img src="https://static.standard.co.uk/s3fs-public/thumbnails/image/2020/02/11/13/ship-shovell-pub.jpg?width=1368"></img>
-              <p>Cute Pubs</p>
+              <Link to="/pubs">
+                <img src="https://static.standard.co.uk/s3fs-public/thumbnails/image/2020/02/11/13/ship-shovell-pub.jpg?width=1368"></img>
+                <p>Cute Pubs</p>
+              </Link>
             </div>
           </div>
         </div>
@@ -55,7 +64,9 @@ const Home = () => {
                 <div className="share-text">
                   <h1>Your world is worth sharing</h1>
                   <p>Turn your pub into your next opportunity</p>
-                  <button className="button">Become a Landlord</button>
+                  <Link to="/landlord">
+                    <button className="button">Become a Landlord</button>
+                  </Link>
                 </div>
               </div>
               <div className="column"></div>
