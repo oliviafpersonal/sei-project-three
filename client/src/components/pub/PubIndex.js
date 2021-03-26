1e2d5d2b040c9165edb87d57b7e4abd23702ab92
atilla-arslan <atillaa@rocketmail.comm>
2021-03-26 11:03:45 +0000

    pubs index further

--- client/src/components/pub/PubIndex.js
+++ client/src/components/pub/PubIndex.js
@@ -22,13 +22,40 @@ const PubIndex = () => {
   return (
     <>
       <Header />
-      <div className="columns">
-        <div className="column">
-          {pubs.map((pub) => (
-            <PubCard key={pub._id} {...pub} />
-          ))}
+      <div className="pub-index-container">
+        <div className="columns">
+          <div className="column">
+            <div className="pub-index-text">
+              <p>{`${pubs.length}+ pubs`}</p>
+              <h3>Pubs in London</h3>
+              <div className="pub-filter-buttons">
+                <button className="pub-filter-button button">
+                  Outside Seating
+                </button>
+                <button className="pub-filter-button button">
+                  Pet friendly
+                </button>
+                <button className="pub-filter-button button">
+                  Food Served
+                </button>
+                <button className="pub-filter-button button">
+                  Live Sports
+                </button>
+                <button className="pub-filter-button button">Ratings</button>
+              </div>
+              <div className="pub-filter-cta">
+                <p>Use filters to refine search of Pubs.</p>
+              </div>
+
+              <hr />
+            </div>
+
+            {pubs.map((pub) => (
+              <PubCard key={pub._id} {...pub} />
+            ))}
+          </div>
+          <div className="column"></div>
         </div>
-        <div className="column"></div>
       </div>
     </>
   )
