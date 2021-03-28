7ac3514772af9a25e8517ec724d0fe46acd4621e
J Edwards <jesse.edwards2@protonmail.com>
2021-03-28 19:28:17 -0400

    removed comments from helper function

--- client/src/helpers/helperFunctions.js
+++ client/src/helpers/helperFunctions.js
@@ -1,8 +1,6 @@
 export const convertTimestamp = (timestamp) => {
   const date = Date.parse(timestamp)
-  console.log('🚀 ~ file: Profile.js ~ line 36 ~ convertTimestamp ~ date', date)
   const newDate = new Date(date)
-  console.log('🚀 ~ file: Profile.js ~ line 38 ~ convertTimestamp ~ newDate', newDate)
   const months = [
     'Jan',
     'Feb',
