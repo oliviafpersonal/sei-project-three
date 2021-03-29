9d5166faf0ecbe9e80b92537b7254c0a7002df48
J Edwards <jesse.edwards2@protonmail.com>
2021-03-28 22:15:05 -0400

    updated review page

--- client/src/components/Forms/Review.js
+++ client/src/components/Forms/Review.js
@@ -1,35 +1,32 @@
-import React from 'react'
-
+/* eslint-disable no-unused-vars */
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
 const Review = () => {
+  const { id } = useParams()
+
+  const [formData, setFormData] = useState({
+    subRating: {
+      price: 0,
+      availability: 0,
+      comfortability: 0,
+    },
+    text: '',
+  })
+
+  const handleChange = (event) => {
+    const newFormData = { ...formData, [event.target.name]: event.target.value }
+    console.log(newFormData)
+    setFormData(newFormData)
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const { data } = await axios.post(`/api/pubs/${id}/reviews`, formData)
+  }
+
   return (
-    <>
-      <div className="field is-horizontal">
-        <div className="field-label is-normal">
-          <label className="label">From</label>
-        </div>
-        <div className="field-body">
-          <div className="field">
-            <p className="control is-expanded has-icons-left">
-              <input className="input" type="text" placeholder="Name" />
-              <span className="icon is-small is-left">
-                <i className="fas fa-user"></i>
-              </span>
-            </p>
-          </div>
-          <div className="field">
-            <p className="control is-expanded has-icons-left has-icons-right">
-              <input className="input is-success" type="email" placeholder="Email" />
-              <span className="icon is-small is-left">
-                <i className="fas fa-envelope"></i>
-              </span>
-              <span className="icon is-small is-right">
-                <i className="fas fa-check"></i>
-              </span>
-            </p>
-          </div>
-        </div>
-      </div>
-  
+    <form onSubmit={handleSubmit}>
       <div className="field is-horizontal">
         <div className="field-label is-normal">
           <label className="label">Price</label>
@@ -38,10 +35,10 @@ const Review = () => {
           <div className="field is-narrow">
             <div className="control">
               <div className="select is-fullwidth">
-                <select>
+                <select onChange={handleChange} name="price">
                   <option value="1">1</option>
                   <option value="2">2</option>
-                  <option value="3">3</option>
+                  <option  defaultValue="3">3</option>
                   <option value="4">4</option>
                   <option value="5">5</option>
                 </select>
@@ -53,16 +50,16 @@ const Review = () => {
   
       <div className="field is-horizontal">
         <div className="field-label is-normal">
-          <label className="label">Comfortability</label>
+          <label className="label" >Comfortability</label>
         </div>
         <div className="field-body">
           <div className="field is-narrow">
             <div className="control">
               <div className="select is-fullwidth">
-                <select>
+                <select onChange={handleChange} name="comfortability">
                   <option value="1">1</option>
                   <option value="2">2</option>
-                  <option value="3">3</option>
+                  <option  defaultValue="3">3</option>
                   <option value="4">4</option>
                   <option value="5">5</option>
                 </select>
@@ -74,16 +71,16 @@ const Review = () => {
   
       <div className="field is-horizontal">
         <div className="field-label is-normal">
-          <label className="label">Availability</label>
+          <label className="label" >Availability</label>
         </div>
         <div className="field-body">
           <div className="field is-narrow">
             <div className="control">
               <div className="select is-fullwidth">
-                <select>
+                <select onChange={handleChange} name="availability">
                   <option value="1">1</option>
                   <option value="2">2</option>
-                  <option value="3">3</option>
+                  <option  defaultValue="3">3</option>
                   <option value="4">4</option>
                   <option value="5">5</option>
                 </select>
@@ -95,12 +92,12 @@ const Review = () => {
   
       <div className="field is-horizontal">
         <div className="field-label is-normal">
-          <label className="label">Question</label>
+          <label className="label">Review</label>
         </div>
         <div className="field-body">
           <div className="field">
             <div className="control">
-              <textarea className="textarea" placeholder="What did you think?"></textarea>
+              <textarea className="textarea" onChange={handleChange} name="text" placeholder="What did you think? "></textarea>
             </div>
           </div>
         </div>
@@ -112,14 +109,14 @@ const Review = () => {
         <div className="field-body">
           <div className="field">
             <div className="control">
-              <button className="button is-primary">
+              <button className="button is-primary" type="submit">
                       Submit Review
               </button>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </form>
 
   )
 }
