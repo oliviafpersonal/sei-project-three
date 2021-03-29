ec7c2cc52afdc6e467526a40a653e7dc1d2301b7
J Edwards <jesse.edwards2@protonmail.com>
2021-03-28 20:29:26 -0400

    created form component

--- client/src/components/pub/PubShow.js
+++ client/src/components/pub/PubShow.js
@@ -16,6 +16,7 @@ import {
 import Header from '../Header'
 import PubComments from './PubComments'
 import { userIsAuthenticated, userIsOwner } from '../../helpers/auth'
+import Review from '../Forms/Review'
 
 const PubShow = () => {
   const params = useParams()
@@ -296,122 +297,7 @@ const PubShow = () => {
 
         <div className="modal-background"></div>
         <div className="modal-content">
-          <div className="field is-horizontal">
-            <div className="field-label is-normal">
-              <label className="label">From</label>
-            </div>
-            <div className="field-body">
-              <div className="field">
-                <p className="control is-expanded has-icons-left">
-                  <input className="input" type="text" placeholder="Name" />
-                  <span className="icon is-small is-left">
-                    <i className="fas fa-user"></i>
-                  </span>
-                </p>
-              </div>
-              <div className="field">
-                <p className="control is-expanded has-icons-left has-icons-right">
-                  <input className="input is-success" type="email" placeholder="Email" />
-                  <span className="icon is-small is-left">
-                    <i className="fas fa-envelope"></i>
-                  </span>
-                  <span className="icon is-small is-right">
-                    <i className="fas fa-check"></i>
-                  </span>
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="field is-horizontal">
-            <div className="field-label is-normal">
-              <label className="label">Price</label>
-            </div>
-            <div className="field-body">
-              <div className="field is-narrow">
-                <div className="control">
-                  <div className="select is-fullwidth">
-                    <select>
-                      <option value="1">1</option>
-                      <option value="2">2</option>
-                      <option value="3">3</option>
-                      <option value="4">4</option>
-                      <option value="5">5</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="field is-horizontal">
-            <div className="field-label is-normal">
-              <label className="label">Comfortability</label>
-            </div>
-            <div className="field-body">
-              <div className="field is-narrow">
-                <div className="control">
-                  <div className="select is-fullwidth">
-                    <select>
-                      <option value="1">1</option>
-                      <option value="2">2</option>
-                      <option value="3">3</option>
-                      <option value="4">4</option>
-                      <option value="5">5</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="field is-horizontal">
-            <div className="field-label is-normal">
-              <label className="label">Availability</label>
-            </div>
-            <div className="field-body">
-              <div className="field is-narrow">
-                <div className="control">
-                  <div className="select is-fullwidth">
-                    <select>
-                      <option value="1">1</option>
-                      <option value="2">2</option>
-                      <option value="3">3</option>
-                      <option value="4">4</option>
-                      <option value="5">5</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="field is-horizontal">
-            <div className="field-label is-normal">
-              <label className="label">Question</label>
-            </div>
-            <div className="field-body">
-              <div className="field">
-                <div className="control">
-                  <textarea className="textarea" placeholder="What did you think?"></textarea>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="field is-horizontal">
-            <div className="field-label">
-            </div>
-            <div className="field-body">
-              <div className="field">
-                <div className="control">
-                  <button className="button is-primary">
-                    Submit Review
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Review />
         </div>
         <button className="modal-close is-large" aria-label="close" onClick={handleToggle}></button>
       </div>
