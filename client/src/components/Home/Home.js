06bdec4dce46819b3707ded33b221114f6385cff
J Edwards <jesse.edwards2@protonmail.com>
2021-03-29 12:30:58 -0400

    updated links

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -36,19 +36,19 @@ const Home = () => {
           </div>
           <div className="columns">
             <div className="column">
-              <Link to="/pubs">
+              <Link to="/pubs/filter-pubs/london">
                 <img src="https://www.telegraphmoorgate.co.uk/-/media/sites/microsites/t/the-telegraph-_-p112/images/2019-ray/drink-gallery/01-telegraph-17092019-93.ashx?w=1024"></img>
                 <p>London</p>
               </Link>
             </div>
             <div className="column">
-              <Link to="/pubs">
+              <Link to="/pubs/filter-pubs/manchester">
                 <img src="https://static.standard.co.uk/s3fs-public/thumbnails/image/2018/12/06/11/the-old-bank-of-england-15.jpg?width=990"></img>
                 <p>Manchester</p>
               </Link>
             </div>
             <div className="column">
-              <Link to="/pubs">
+              <Link to="/pubs/filter-pubs/cardiff">
                 <img src="https://static.standard.co.uk/s3fs-public/thumbnails/image/2020/02/11/13/ship-shovell-pub.jpg?width=1368"></img>
                 <p>Cardiff</p>
               </Link>
