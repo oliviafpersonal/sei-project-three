3cb31913fbbde8ef1e3d859998529d8082cca80f
J Edwards <jesse.edwards2@protonmail.com>
2021-03-28 23:52:15 -0400

    created working modal function

--- client/src/helpers/helperFunctions.js
+++ client/src/helpers/helperFunctions.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export const convertTimestamp = (timestamp) => {
   const date = Date.parse(timestamp)
   const newDate = new Date(date)
@@ -17,4 +19,18 @@ export const convertTimestamp = (timestamp) => {
   ]
 
   return (months[newDate.getMonth()] + ' ' + newDate.getFullYear())
+}
+
+export const displayModal = (toggle, Component, toggleController) => {
+  return (
+    <>
+      <div className={`modal ${toggle && 'is-active'}`}>
+        <div className="modal-background"></div>
+        <div className="modal-content">
+          < Component />
+        </div>
+        <button className="modal-close is-large" onClick={toggleController} aria-label="close"></button>
+      </div>
+    </>
+  )
 }
