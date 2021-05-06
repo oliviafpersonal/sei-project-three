378e6c06c23e53539542ae8768284b47ba3ecbd4
atilla-arslan <atillaa@rocketmail.comm>
2021-05-06 17:43:56 +0100

    changes

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -1,3 +1,10 @@
-export const port = 4000
-export const dbURI = 'mongodb://localhost/pub-hub'
-export const secret = 'Keep it secret, keep it safe'
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/pub-hub'
+const port = process.env.PORT || 4000
+const secret = process.env.SECRET || 'shhhh its a secret'
+
+//prettier-ignore
+module.exports = {
+  dbURI,
+  port,
+  secret
+}
